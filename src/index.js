3d2d91a5b10f89af8b69668cb968efbba479bfcd
Diego Ramiro Zuleta <dzuleta@santandertecnologia.com.ar>
2020-03-25 17:45:15 -0300

    test lectura de archivos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();app.listen(3000, () => {
  console.log("El servidor está inicializado en el puerto 3000");
@@ -7,3 +8,12 @@ const app = express();app.listen(3000, () => {
 app.get('/', (req, res) => {
   res.send("Estoy corriendo en okd")
 });
+
+app.get('/read', (req, res) => {
+  try {
+    const data = fs.readFileSync("/my-files/data.txt");
+    res.send(data.toString());
+  } catch (error) {
+    console.log(error);
+  }
+})
